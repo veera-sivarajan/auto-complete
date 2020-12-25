0672fa5014de57c8234c8d4de08bd0b3af7f6ddd
Veera Sivarajan <sveera.2001@gmail.com>
2020-12-25 19:21:07 +0530

    Added: create_node() function to return a node for give char

--- trie.c
+++ trie.c
@@ -1,5 +1,31 @@
 # include <stdio.h>
+# include <stdlib.h>
 
-void main () {
-    printf("Hello, Auto-Complete\n");
+# define NUM_CHILD 26
+
+typedef struct Node {
+    char letter;
+    struct Node *childrens[NUM_CHILD];
+} node;
+
+
+node *create_node (char letter) {
+    node *temp = (node *) malloc(sizeof(node));
+    temp->letter = letter;
+    for (int i = 0; i < NUM_CHILD; ++i) {
+        temp->childrens[i] = NULL;
+    }
+    return temp;
+}
+    
+
+
+
+
+
+
+int main () {
+    node *temp = create_node('h');
+    printf("Letter: %c\n", temp->letter);
+    return 0;
 }
