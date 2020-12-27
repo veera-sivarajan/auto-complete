f4ec824f4698a871f010d0b162385aa74213957e
Veera Sivarajan <sveera.2001@gmail.com>
2020-12-27 09:39:56 +0530

    FIXME: new letters overwrites existing letter's data

--- trie.c
+++ trie.c
@@ -1,4 +1,5 @@
 # include "trie.h"
+
 int i = 0;
 node *create_node (char letter, int num) {
     node *temp = (node *) malloc(sizeof(node));
@@ -25,20 +26,21 @@ node *get_next (node *parent, char letter) {
 
 void join_nodes (node *parent, node *child) {
     int index = get_index(child->letter);
-    if (parent->childrens[index] == NULL) 
-        parent->childrens[index] = child;
+    parent->childrens[index] = child;
 }
 
-// FIXME: strings with same starting letter are inserted incorrectly
+// FIXME: new letters overwrites existing letter's EOW data 
 void insert_word (node *root, char *word) {
     int len = strlen(word);
     node *temp = root;
     for (int i = 0; i < len; ++i) {
         if (i == len - 1) {
+            printf("INSERTING LAST ELE: %c\n", word[i]);
             join_nodes(temp, create_node(word[i], 1));
             // temp = get_next(temp, word[i]);
         }
         else {
+            printf("INSERTING: %c\n", word[i]);
             join_nodes(temp, create_node(word[i], 0));
             temp = get_next(temp, word[i]);
         }
@@ -54,13 +56,13 @@ void traverse_helper (int start_index, node *root) {
     for (int i = start_index; i < NUM_CHILD; ++i) {
         node *child = root->childrens[i];
         if (child) {
+            printf("%c", child->letter);
             if (child->EOW == 1) {
-                printf("%c", child->letter);
                 printf("\n");
                 // traverse_helper(get_index(child->letter) + 1, root);
+                continue;
             }
             else if (child->EOW == 0) {
-                printf("%c", child->letter);
                 traverse_helper(0, child);
             }
         }
