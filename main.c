110957bedfaa9bf47b6e35ce19c680ab71e9ced7
Veera Sivarajan <sveera.2001@gmail.com>
2021-01-05 11:05:19 +0530

    fixed most memory leaks

--- main.c
+++ main.c
@@ -3,22 +3,27 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
+# define NUM_WORDS 39
+
 int main (void) {
     node *root = create_node();
     char **words = read_file("input.txt");
-    int i = 0;
-    while (i < 39) {
+    char *input;
+    for (int i = 0; i < NUM_WORDS; ++i) {
         insert_word(root, words[i]);
-        ++i;
     }
-    free(words);
     while (1) {
-        char *input = readline(">> ");
+        input = readline(">> ");
         if (strcmp(input, "exit") == 0) {
             break;
         }
         auto_complete(root, input); 
+        free(input);
     }
+    for (int i = 0; i < NUM_WORDS; ++i) {
+        free(words[i]);
+    }
+    free(words);
     free_tree(root);
     free(root);
     return 0;
